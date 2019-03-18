d1b76e250efbff91e9dac334ebc2270d8a724e93
akram09 <a.boutouchent@esi-sba.dz>
2019-03-18 23:47:42 +0100

    adding fabric to the app (a crashylitic library ) it report you if a crash happened in the app

--- app/src/main/java/com/roacult/kero/oxxy/projet2eme/AndroidApplication.kt
+++ app/src/main/java/com/roacult/kero/oxxy/projet2eme/AndroidApplication.kt
@@ -1,13 +1,16 @@
 package com.roacult.kero.oxxy.projet2eme
 
+import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import com.roacult.kero.oxxy.projet2eme.di.component.DaggerAppComponent
+import io.fabric.sdk.android.Fabric
 
 class AndroidApplication :DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         Stetho.initializeWithDefaults(this)
+        Fabric.with(this  , Crashlytics())
         return  DaggerAppComponent.builder().create(this)
     }
 }
