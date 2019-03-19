7b49904f6009b078238e1774848fecc109491e0a
akram09 <a.boutouchent@esi-sba.dz>
2019-03-19 14:56:52 +0100

    Adding instabug a tool for reporting bugs and adding features

--- app/src/main/java/com/roacult/kero/oxxy/projet2eme/AndroidApplication.kt
+++ app/src/main/java/com/roacult/kero/oxxy/projet2eme/AndroidApplication.kt
@@ -6,11 +6,18 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import com.roacult.kero.oxxy.projet2eme.di.component.DaggerAppComponent
 import io.fabric.sdk.android.Fabric
+import com.instabug.library.invocation.InstabugInvocationEvent
+import com.instabug.library.Instabug
+
+
 
 class AndroidApplication :DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         Stetho.initializeWithDefaults(this)
         Fabric.with(this  , Crashlytics())
+        Instabug.Builder(this, "bd7ff81db1b61cb2ec5f4008c4b2ad9b")
+            .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
+            .build()
         return  DaggerAppComponent.builder().create(this)
     }
 }
