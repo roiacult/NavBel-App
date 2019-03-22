aab6a432eaaeb300615fb931e57774e504d9e658
akram09 <a.boutouchent@esi-sba.dz>
2019-03-22 02:10:04 +0100

    ADDING FIREBASE MESSAGING FIRST TRY

---
+++ app/src/main/java/com/roacult/kero/oxxy/projet2eme/utils/pushnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,7 @@
+package com.roacult.kero.oxxy.projet2eme.utils.pushnotification
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+}
