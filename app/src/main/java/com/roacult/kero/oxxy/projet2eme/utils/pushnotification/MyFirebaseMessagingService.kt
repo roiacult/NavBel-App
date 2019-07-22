5e230d6e40c76f2192ae25ba38132cd7d3397b5e
akram09 <a.boutouchent@esi-sba.dz>
2019-07-22 17:34:33 +0100

    configure notification

--- app/src/main/java/com/roacult/kero/oxxy/projet2eme/utils/pushnotification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/roacult/kero/oxxy/projet2eme/utils/pushnotification/MyFirebaseMessagingService.kt
@@ -1,7 +1,12 @@
 package com.roacult.kero.oxxy.projet2eme.utils.pushnotification
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
-
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+        Log.e("errr", p0?.notification?.title ?:" Erroooooorrr")
+    }
 }
