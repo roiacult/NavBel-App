1db63f1b8c139206e6498eff5963c18fc4f18e27
akram09 <a.boutouchent@esi-sba.dz>
2019-07-28 18:27:06 +0100

    fix award bug

--- data/src/main/java/com/roacult/kero/oxxy/projet2eme/network/entities/Awards.kt
+++ data/src/main/java/com/roacult/kero/oxxy/projet2eme/network/entities/Awards.kt
@@ -2,5 +2,5 @@ package com.roacult.kero.oxxy.projet2eme.network.entities
 
 data class AwardRemote(val id:Int , val image:String , val point:Int , val description:String)
 data class AwardsRemote(val data:List<AwardRemote>)
-data class GetAwardRemote(val userId:Int , val rewardid:Int)
+data class GetAwardRemote(val id:Int , val rewardid:Int)
 data class GetAwardResponse(val response:Int)
